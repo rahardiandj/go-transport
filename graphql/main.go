e90049db22d5ddb17502b09e2844acde7e51af03
Rahardian Juliarto <rahardian.juliarto@traveloka.com>
2019-08-26 18:11:24 +0700

    Create init project for go-graphql

---
+++ graphql/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/go-chi/chi"
+)
+
+func main() {
+
+}
+
+func initializeAPI() {
+	router := chi.NewRouter()
+
+}
---
+++ graphql/postgres/postgres.go
@@ -0,0 +1,5 @@
+package postgres
+
+func test() {
+
+}
