fee446a27f555e7412c0a74e541c316580b2b6fc
Rahardian Juliarto <rahardian.juliarto@traveloka.com>
2019-08-28 16:29:55 +0700

    Finished all code and add realize file

--- graphql/main.go
+++ graphql/main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
+
+	"github.com/rahardiandj/go-transport/graphql/server"
+
+	"github.com/graphql-go/graphql"
 
 	"github.com/go-chi/chi"
+	"github.com/rahardiandj/go-transport/graphql/gql"
 	"github.com/rahardiandj/go-transport/graphql/postgres"
 )
 
 func main() {
 
+	router, db := initializeAPI()
+	defer db.Close()
+
+	log.Fatal(http.ListenAndServe(":8889", router))
 }
 
-func initializeAPI() {
+func initializeAPI() (*chi.Mux, *postgres.Db) {
 	router := chi.NewRouter()
 
 	db, err := postgres.New(
@@ -21,5 +35,32 @@ func initializeAPI() {
 	if err != nil {
 		log.Fatalf("could not connect to postgres : %v", err)
 	}
+
+	rootQuery := gql.NewRoot(db)
+
+	sc, err := graphql.NewSchema(
+		graphql.SchemaConfig{Query: rootQuery.Query},
+	)
+
+	if err != nil {
+		fmt.Println("error creating schema: %v ", err)
+	}
+
+	s := server.Server{
+		GqlSchema: &sc,
+	}
+
+	//add middleware to the router
+	router.Use(
+		render.SetContentType(render.ContentTypeJSON),
+		middleware.Logger,
+		middleware.DefaultCompress,
+		middleware.StripSlashes,
+		middleware.Recoverer, // recover from panic witout crashing server
+	)
+
+	router.Post("/graphql", s.GraphQL())
+
+	return router, db
 
 }
--- graphql/server/server.go
+++ graphql/server/server.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/render"
+	"github.com/rahardiandj/go-transport/graphql/gql"
 
 	"github.com/graphql-go/graphql"
 )
